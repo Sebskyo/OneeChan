eb64b7199a96a66cdedf9c2b0a7e952a60b84c31
nebukazar <justsoulay@gmail.com>
2016-09-22 17:07:14 +0100

    Move things around
    Adding .catalog-mode condition although it doesn't work 100% of the time
    due topage not refreshing.

--- src/style.js
+++ src/style.js
@@ -4,10 +4,10 @@
 <%= grunt.file.read('src/css/Colors.css') %>
 <%= grunt.file.read('src/css/Fonts.css') %>
 <%= grunt.file.read('src/css/Quickreply.css') %>
-" + ($SS.conf["Disable In Catalog View"] && $SS.location.catalog ? $SS.conf["Sidebar Position"] == 3 : ($SS.conf["Sidebar Position"] !== 3 ? "<%= grunt.file.read('src/css/Sidebar.css') %>" : "")) + "
+" + ($SS.conf["Disable In Catalog View"] && $SS.location.catalog || $(":root.catalog-mode").exists() ? $SS.conf["Sidebar Position"] == 3 : ($SS.conf["Sidebar Position"] !== 3 ? "<%= grunt.file.read('src/css/Sidebar.css') %>" : "")) + "
 <%= grunt.file.read('src/css/Icons.css') %>
 <%= grunt.file.read('src/css/Highlight.css') %>
-<%= grunt.file.read('src/css/Options.css') %>
 <%= grunt.file.read('src/css/Catalog.css') %>
+<%= grunt.file.read('src/css/Options.css') %>
 " + ($SS.conf["Version Fix"] == 3 ? "<%= grunt.file.read('src/forks/loadletter.css') %>" : "") + "
 " + $SS.theme.customCSS + "
