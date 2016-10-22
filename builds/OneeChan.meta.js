f99ea2a9c966fe053014f0980068abe2a217b6ba
nebu <justsoulay@gmail.com>
2016-10-22 20:37:51 +0100

    Release OneeChan v5.5.50

--- builds/OneeChan.meta.js
+++ builds/OneeChan.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         OneeChan
-// @version      5.5.49
+// @version      5.5.50
 // @namespace    OneeChan
 // @description  Customizable rice and themes for 4chan X.
 // @minGMVer     1.15
